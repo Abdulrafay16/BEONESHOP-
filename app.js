4899380142ce667b4acb42124a7f58b1ba0aaa5c
Abdulrafay16 <99479946+Abdulrafay16@users.noreply.github.com>
2022-05-24 12:55:23 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,9 @@
+var username =prompt(`Enter Your Name`);
+var age = +prompt(`Enter Your Age`);
+if(age > 18 <=18);{
+    alert(`Welcome To beoneshopone`)
+}
+
+// else{
+//     alert(`You are not allowed`)
+// }
